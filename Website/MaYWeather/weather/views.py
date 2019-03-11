e74cad0bcc9635d362d704925c32d5e3d866ac8a
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-03-11 23:03:46 +0530

    Used Open Weather API

--- Website/MaYWeather/weather/views.py
+++ Website/MaYWeather/weather/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+import requests
+import json
+from . import api
 
 def index(request):
+
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid={}".format(api.api_key)
+    r = requests.get(url)
+
+    r = r.json()
+
+    print(r)
+
     return render(request, 'weather/index.html')
