136b74bc5dd5ceb80bf940a8229c197c7f1fc543
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-03-11 22:36:34 +0530

    Basic Setup

--- Website/MaYWeather/MaYWeather/settings.py
+++ Website/MaYWeather/MaYWeather/settings.py
@@ -14,8 +14,6 @@ import os
 
 from . import secret_settings
 
-print(secret_settings.secret_key)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -41,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
