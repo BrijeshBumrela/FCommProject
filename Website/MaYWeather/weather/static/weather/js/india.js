6437b48a9f5637f8498272c898ea9952da0a02a3
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-03-12 10:45:59 +0530

    Added chart and dashboard

---
+++ Website/MaYWeather/weather/static/weather/js/india.js
@@ -0,0 +1,185 @@
+// Prepare demo data
+// Data is joined to map using value of 'hc-key' property by default.
+// See API docs for 'joinBy' for more info on linking data and map.
+var IndiaData = [
+    ['in-py', 0],
+    ['in-ld', 1],
+    ['in-wb', 2],
+    ['in-or', 3],
+    ['in-br', 4],
+    ['in-sk', 5],
+    ['in-ct', 6],
+    ['in-tn', 7],
+    ['in-mp', 8],
+    ['in-2984', 9],
+    ['in-ga', 10],
+    ['in-nl', 11],
+    ['in-mn', 12],
+    ['in-ar', 13],
+    ['in-mz', 14],
+    ['in-tr', 15],
+    ['in-3464', 16],
+    ['in-dl', 17],
+    ['in-hr', 18],
+    ['in-ch', 19],
+    ['in-hp', 20],
+    ['in-jk', 21],
+    ['in-kl', 22],
+    ['in-ka', 23],
+    ['in-dn', 24],
+    ['in-mh', 25],
+    ['in-as', 26],
+    ['in-ap', 27],
+    ['in-ml', 28],
+    ['in-pb', 29],
+    ['in-rj', 30],
+    ['in-up', 31],
+    ['in-ut', 32],
+    ['in-jh', 33]
+];
+
+
+// Create the chart
+Highcharts.mapChart('india-map', {
+    chart: {
+        map: 'countries/in/in-all'
+    },
+
+    title: {
+        text: 'Highmaps basic demo'
+    },
+
+    subtitle: {
+        text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/in/in-all.js">India</a>'
+    },
+
+    mapNavigation: {
+        enabled: true,
+        buttonOptions: {
+            verticalAlign: 'bottom'
+        }
+    },
+
+    colorAxis: {
+        min: 0
+    },
+
+    series: [{
+        point: {
+            events: {
+                click: function() {
+                    console.log(this["hc-key"]);
+                    renderState(this["hc-key"], 'state-map')
+                }
+            }
+        },
+        data: IndiaData,
+        name: 'Random data',
+        states: {
+            hover: {
+                color: '#BADA55'
+            }
+        },
+        dataLabels: {
+            enabled: true,
+            format: '{point.name}'
+        }
+    }]
+});
+
+
+function renderState(state, id) {
+    Highcharts.mapChart(id, {
+        chart: {
+            map: 'countries/in/in-mp'
+        },
+
+        title: {
+            text: 'Highmaps basic demo'
+        },
+
+        subtitle: {
+            text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/in/in-all.js">India</a>'
+        },
+
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
+            }
+        },
+
+        colorAxis: {
+            min: 0
+        },
+
+        series: [{
+            data: getStateData(state),
+            name: 'Random data',
+            states: {
+                hover: {
+                    color: '#BADA55'
+                }
+            },
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }]
+    });
+}
+
+
+Highcharts.chart('requests', {
+
+    title: {
+        text: 'No Of Request made by Our APP (Monthwise)'
+    },
+
+    subtitle: {
+        text: 'Source: thesolarfoundation.com'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Number of Requests'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2015
+        }
+    },
+
+    series: [{
+        name: 'Installation',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});
+
+
--- Website/MaYWeather/weather/views.py
+++ Website/MaYWeather/weather/views.py
@@ -7,11 +7,11 @@ from . import api
 def index(request):
 
 
-    url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid={}".format(api.api_key)
-    r = requests.get(url)
+    # url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid={}".format(api.api_key)
+    # r = requests.get(url)
 
-    r = r.json()
+    # r = r.json()
 
-    print(r)
+    # print(r)
 
     return render(request, 'weather/index.html')
